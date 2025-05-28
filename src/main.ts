f92b954b982c74e4fe0bfe73c6752691de239c7d
mancarius <mattia.mancarella.dev@gmail.com>
2025-05-28 16:24:38 +0200

    feat: add Firebase integration and deployment script
    - Updated package.json to include Firebase as a dependency and added a deploy script for Firebase hosting.
    - Modified main.ts to initialize Firebase app and analytics with the provided configuration.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDgUEZFABbdK7bzd65w_b7-LGGax9WFVw",
+  authDomain: "ia-al-lavoro.firebaseapp.com",
+  projectId: "ia-al-lavoro",
+  storageBucket: "ia-al-lavoro.firebasestorage.app",
+  messagingSenderId: "1053511788535",
+  appId: "1:1053511788535:web:9c96b6301f8b292ec3b0f2",
+  measurementId: "G-RFVKMS2DGP"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
