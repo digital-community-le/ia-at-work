031826636406b7328d9bdd27fba68385a049b074
mancarius <mattia.mancarella.dev@gmail.com>
2025-06-30 14:09:38 +0200

    feat: aggiungi sponsor Hybris con logo e descrizione, migliora la sezione sponsor

---
+++ src/app/constants/sponsors.ts
@@ -0,0 +1,8 @@
+export const SPONSORS = [
+  {
+    name: 'Hybris Hub',
+    logo: '/images/sponsors/hybris/Hybris_Logo_Picto_Green+Typo_Black.webp',
+    url: 'https://hybrissoftware.it/',
+    description: 'Hybris Hub is a software development company that specializes in building digital products with a focus on user experience and performance.'
+  }
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { FeatureToggleService } from '../../services/feature-toggle.service';
 import { NgOptimizedImage } from '@angular/common';
+import { SPONSORS } from '../../constants/sponsors';
 
 @Component({
   selector: 'app-home',
@@ -16,4 +17,6 @@ import { NgOptimizedImage } from '@angular/common';
 })
 export class HomeComponent {
   protected readonly featureToggleService = inject(FeatureToggleService);
+
+  protected readonly sponsors = [...SPONSORS].sort((a, b) => a.name.localeCompare(b.name));
 }
